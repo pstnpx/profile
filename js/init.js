0099a301066e18e1563ddd6d88433416e8175494
pstnpx <ps.tanupat@gmail.com>
2023-04-20 00:41:08 +0700

    Commit summary
    Update work experience

--- js/init.js
+++ js/init.js
@@ -10,3 +10,28 @@ window.addEventListener('scroll', function() {
     skillBarItems.forEach(item => item.classList.remove('show'));
   }
 });
+
+if (window.innerWidth >= 1024) { // Only execute on desktop devices
+  window.addEventListener('scroll', function() {
+    const resumeDiv = document.getElementById('resume-job');
+    const distanceFromTop = resumeDiv.getBoundingClientRect().top;
+    if (distanceFromTop < window.innerHeight * 0.5) {
+      resumeDiv.style.height = 'margin-bottom: var(--vspace-2);'; /* Change this to the expanded height you want */
+    } else {
+      resumeDiv.style.height = 'margin-bottom: var(--vspace-1_75);'; /* Change this to the initial height you set earlier */
+    }
+  });
+}
+
+const expandButton = document.querySelector('.expand-button');
+const expandableContent = document.querySelector('.expandable-content');
+
+expandButton.addEventListener('click', () => {
+  expandableContent.classList.toggle('expanded');
+  if (expandableContent.classList.contains('expanded')) {
+    expandButton.textContent = 'Collapse';
+  } else {
+    expandButton.textContent = 'Expand';
+  }
+});
+
